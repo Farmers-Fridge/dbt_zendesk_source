ba98326b861ab1453243834d2f581d2fb7e27563
Jeff Chen <jeffchen@ip-172-16-10-15.us-east-2.compute.internal>
2020-10-13 14:32:55 -0500

    modified meta tags and column selection syntax for keyworld collision

--- models/src_zendesk_ticket_comment.sql
+++ models/src_zendesk_ticket_comment.sql
@@ -8,7 +8,7 @@ with base as (
     select
 
       id as ticket_comment_id,
-      _fivetran_synced,
+      -- _fivetran_synced,
       body,
       created as created_at,
       public as is_public,
@@ -16,7 +16,18 @@ with base as (
       user_id,
       facebook_comment as is_facebook_comment,
       tweet as is_tweet,
-      voice_comment as is_voice_comment
+      voice_comment as is_voice_comment,
+      trusted as is_trusted,
+      voice_comment_transcription_visible as is_voice_comment_transcription_visible,
+      transcription_status,
+      recording_url,
+      started_at,
+      "FROM" as from_number,
+      location,
+      "TO" as to_number,
+      call_duration,
+      call_id,
+      transcription_text
 
     from base
 
